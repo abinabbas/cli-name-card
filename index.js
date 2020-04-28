9b2c5907fb9b9807e75f6309dbabc0bc2a516e16
Abin Abbas <ryzm10.abin@gmail.com>
2020-04-28 06:01:53 +0530

    added terminal image

--- index.js
+++ index.js
@@ -1,54 +1,51 @@
 #!/usr/bin/env node
 
-const chalk = require('chalk')
-const boxen = require('boxen')
+const chalk = require("chalk");
+const boxen = require("boxen");
+const terminalImage = require("terminal-image");
 
-// Options for Boxen
-const options = {
-  padding: 1,
-  margin: 1,
-  borderStyle: 'round',
-}
+(async () => {
 
-// Text + chalk
-const data = {
-  name: chalk.white('Abin Abbas /'),
-  handle: chalk.cyan('abinabbas'),
-  work: chalk.white('Head Geek at Datameris'),
-  github: chalk.cyan('github.com/abinabbas'),
-  linkedin: chalk.cyan('linkedin.com/in/abinabbas'),
-  web: chalk.cyan('abinabbas.com'),
-  npx: chalk.cyan('npx abinabbas'),
-  labelWork: chalk.white.bold('      Work:'),
-  labelGitHub: chalk.white.bold('    GitHub:'),
-  labelLinkedIn: chalk.white.bold('  LinkedIn:'),
-  labelWeb: chalk.white.bold('       Web:'),
-  labelCard: chalk.white.bold('      Card:'),
-}
+  // Options for boxen
+  const options = {
+    padding: 1,
+    margin: 1,
+    borderStyle: "round",
+    backgroundColor: "blue"
+  };
 
-// Creating String
-const newline = '\n'
-const heading = `${data.name} ${data.handle}`
-const working = `${data.labelWork}  ${data.work}`
-const githubing = `${data.labelGitHub}  ${data.github}`
-const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`
-const webing = `${data.labelWeb}  ${data.web}`
-const carding = `${data.labelCard}  ${data.npx}`
+  // Text + chalk
+  const user = {
+    name: chalk.white("Abin Abbas /"),
+    handle: chalk.cyan("abinabbas"),
+    work: chalk.white("Head Geek at Datameris"),
+    github: chalk.cyan("github.com/abinabbas"),
+    linkedin: chalk.cyan("linkedin.com/in/abinabbas"),
+    mail: chalk.white("abinabbas@datameris.com"),
+    phone: chalk.white("+91-9946-986806"),
+    web: chalk.cyan("abinabbas.com"),
+    labelWork: chalk.white.bold("      Work:"),
+    labelGitHub: chalk.white.bold("    GitHub:"),
+    labelLinkedIn: chalk.white.bold("  LinkedIn:"),
+    labelMail: chalk.white.bold("     Email:"),
+    labelPhone: chalk.white.bold("     Phone:"),
+    labelWeb: chalk.white.bold("       Web:")
+  };
 
-// Put all together into a single variable to boxen effectively
-const output =
-  heading +
-  newline +
-  newline +
-  working +
-  newline +
-  githubing +
-  newline +
-  linkedining +
-  newline +
-  webing +
-  newline +
-  newline +
-  carding
+  //Creating Output
+  const output = `
+  ${user.name} ${user.handle}
 
-console.log(chalk.green(boxen(output, options)))
+  ${user.labelWork}  ${user.work}
+  ${user.labelGitHub}  ${user.github}
+  ${user.labelLinkedIn}  ${user.linkedin}
+  ${user.labelMail}  ${user.mail}
+  ${user.labelPhone}  ${user.phone}
+
+  ${user.labelWeb}  ${user.web}
+  `;
+
+  console.log('\n');
+  console.log(await terminalImage.file("./assets/dp/abinabbas.jpg"));
+  console.log(chalk.grey(boxen(output, options)));
+})();
